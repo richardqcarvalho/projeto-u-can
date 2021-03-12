0bf334ac45dca677c9952016bb2655b4d60d40fd
Richard Carvalho <richardcarvalhooo@gmail.com>
2021-03-12 14:18:26 -0300

    refactor: Remodeling typings

--- back-end/src/models/User.ts
+++ back-end/src/models/User.ts
@@ -1,6 +1,30 @@
+import client from '../database/connection';
+import Sequelize from 'sequelize';
+
 export type UserProps = {
-  CODIGO: number;
-  NOME: string;
-  DATANASCIMENTO: string;
-  FOTO: string;
+  code?: number;
+  name: string;
+  birthDate: string;
+  photo?: string;
 };
+
+export const UserModel = client.define('User', {
+  code: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  birthDate: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  photo: {
+    type: Sequelize.BLOB('long'),
+    allowNull: true,
+  },
+});
