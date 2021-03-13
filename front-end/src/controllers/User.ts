ff203ea71cef47f360672a651f5126ee7c95c5ce
Richard Carvalho <richardcarvalhooo@gmail.com>
2021-03-13 19:25:35 -0300

    refactor: Dividing responsabilities

--- front-end/src/controllers/User.ts
+++ front-end/src/controllers/User.ts
@@ -1,29 +1,9 @@
 import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { LoginProps } from '../models/Login';
-import { CreateUserProps, UserProps } from '../models/User';
+import { UserProps } from '../models/User';
 import api from '../services/api';
 
 const UsersController = () => {
-  const history = useHistory();
   const [users, setUsers] = useState<UserProps[]>([]);
-  const [emptyError, setEmptyError] = useState<boolean>(false);
-  const [userError, setUserError] = useState<boolean>(false);
-
-  const logIn: LoginProps = async ({ code }) => {
-    setEmptyError(false);
-    if (!code) {
-      setEmptyError(true);
-    } else {
-      const { data } = await api.get(`/users/${code}`);
-      if (data) {
-        localStorage.setItem('code', code);
-        history.push('/');
-      } else {
-        openUserErrorModal();
-      }
-    }
-  };
 
   const getUsers = async () => {
     const { data } = await api.get('/users');
@@ -31,32 +11,9 @@ const UsersController = () => {
     setUsers(data);
   };
 
-  const createUser: CreateUserProps = async ({ name, birthDate }) => {
-    const { data } = await api.post('/users', {
-      name,
-      birthDate,
-    });
-
-    return data;
-  };
-
-  const openUserErrorModal = () => {
-    setUserError(true);
-  };
-
-  const closeUserErrorModal = () => {
-    setUserError(false);
-  };
-
   return {
-    emptyError,
-    userError,
     users,
-    logIn,
-    createUser,
     getUsers,
-    openUserErrorModal,
-    closeUserErrorModal,
   };
 };
 
