8e236b896409cf7e1369f41b88d259c8c5a5f5fc
Richard Carvalho <richardcarvalhooo@gmail.com>
2021-03-13 19:26:11 -0300

    refactor: Implementing divisions on view

--- front-end/src/pages/Login/index.tsx
+++ front-end/src/pages/Login/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
-import UsersController from '../../controllers/User';
+import LoginController from '../../controllers/Login';
 import {
   Container,
   Wrapper,
@@ -10,73 +10,74 @@ import {
   ErrorMessage,
   Modal,
   Text,
-  CloseButton,
 } from './styles';
 
 let code: string;
 
 const Login: React.FC = () => {
   const history = useHistory();
-  const { logIn, emptyError, userError } = UsersController();
+  const { logIn, emptyError, userError, borderWidth } = LoginController();
 
   return (
-    <Container
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: {
-          duration: 0.5,
-        },
-      }}
-    >
+    <>
       <Modal
-        initial={{ scale: 0 }}
+        initial={{ y: -20 }}
         animate={{
-          scale: 1,
+          y: 0,
           transition: {
             duration: 1.5,
           },
         }}
+        exit={{ y: -20 }}
         style={{ display: `${userError ? 'flex' : 'none'}` }}
       >
-        <CloseButton
-          whileHover={{
-            scale: 1.1,
-          }}
-          whileTap={{
-            scale: 0.9,
-          }}
-          onClick={{}}
-        />
         <Text>Usuário não encontrado. Tente novamente!</Text>
       </Modal>
-      <Wrapper>
-        <Input
-          type="text"
-          onChange={e => (code = e.target.value)}
-          onKeyPress={event => {
-            if (event.key === 'Enter') logIn({ code });
-          }}
-        />
-        <ErrorMessage
-          style={{ display: `${emptyError ? 'flex' : 'none'}` }}
-          initial={{ y: -30 }}
-          animate={{
-            y: 0,
-            transition: {
-              duration: 1.5,
-            },
-          }}
-        >
-          Por favor, preencha este campo!
-        </ErrorMessage>
-        <Button label="Entrar" onClick={() => logIn({ code })} />
-      </Wrapper>
-      <Divider />
-      <Wrapper>
-        <Button label="Criar conta" onClick={() => history.push('/sign-up')} />
-      </Wrapper>
-    </Container>
+      <Container
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: {
+            duration: 0.5,
+          },
+        }}
+      >
+        <Wrapper>
+          <Input
+            type="text"
+            onChange={e => (code = e.target.value)}
+            onKeyPress={event => {
+              if (event.key === 'Enter') logIn({ code });
+            }}
+            placeholder="Digite seu id"
+            style={{
+              borderWidth: borderWidth(),
+              borderColor: 'red',
+            }}
+          />
+          <ErrorMessage
+            style={{ display: `${emptyError ? 'flex' : 'none'}` }}
+            initial={{ y: -30 }}
+            animate={{
+              y: 0,
+              transition: {
+                duration: 1.5,
+              },
+            }}
+          >
+            Por favor, preencha este campo!
+          </ErrorMessage>
+          <Button label="Entrar" onClick={() => logIn({ code })} />
+        </Wrapper>
+        <Divider />
+        <Wrapper>
+          <Button
+            label="Criar conta"
+            onClick={() => history.push('/sign-up')}
+          />
+        </Wrapper>
+      </Container>
+    </>
   );
 };
 
--- front-end/src/pages/SignUp/index.tsx
+++ front-end/src/pages/SignUp/index.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import SignUpController from '../../controllers/SignUp';
+import { Wrapper, ErrorMessage } from './styles';
 
-import { Container } from './styles';
+let name: string;
+let birthDate: string;
 
 const SignUp: React.FC = () => {
+  const { signUp, borderWidth, birthDateEmpty, nameEmpty } = SignUpController();
+
   return (
-    <Container>
-      <h1>SignUp</h1>
-    </Container>
+    <Wrapper>
+      <Input
+        type="text"
+        onChange={e => (name = e.target.value)}
+        onKeyPress={event => {
+          if (event.key === 'Enter') signUp({ name, birthDate });
+        }}
+        placeholder="Digite seu nome"
+        style={{
+          borderWidth: borderWidth(nameEmpty),
+          borderColor: 'red',
+        }}
+      />
+      <Input
+        type="date"
+        onChange={e => (birthDate = e.target.value)}
+        style={{
+          borderWidth: borderWidth(birthDateEmpty),
+          borderColor: 'red',
+        }}
+      />
+      <ErrorMessage
+        style={{ display: `${birthDateEmpty || nameEmpty ? 'flex' : 'none'}` }}
+        initial={{ y: -30 }}
+        animate={{
+          y: 0,
+          transition: {
+            duration: 1.5,
+          },
+        }}
+      >
+        Por favor, preencha este campo!
+      </ErrorMessage>
+      <Button label="Criar conta" onClick={() => signUp({ name, birthDate })} />
+    </Wrapper>
   );
 };
 
